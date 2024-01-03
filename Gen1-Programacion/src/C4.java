c3ea38abb06c7b76d714ded8c8f7b4bd919fad4a
unknown <erwinlopez177@gmail.com>
2024-01-03 13:03:48 -0600

    Se sube un cuarto programa donde se emplea el ciclo for

---
+++ Gen1-Programacion/src/C4.java
@@ -0,0 +1,20 @@
+public class C4 {
+    public static void main(String[] args) {
+        //crear un programa en java que muestr la tabla de multiplicar del 5
+        for(int i = 0; i<10; i++) {
+            System.out.println("5 X " + (i + 1) + " = " + ((i + 1) * 5));
+
+            /*System.out.println("5 X 1 = 5");
+            System.out.println("5 X 2 = 10");
+            System.out.println("5 X 3 = 15");
+            System.out.println("5 X 4 = 20");
+            System.out.println("5 X 5 = 25");
+            System.out.println("5 X 6 = 30");
+            System.out.println("5 X 7 = 35");
+            System.out.println("5 X 8 = 40");
+            System.out.println("5 X 9 = 45");
+            System.out.println("5 X 10 = 50");*/
+        }
+    }
+}
+
