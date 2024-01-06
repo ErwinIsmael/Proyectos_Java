bdfcbcdeecf672821a02c6e337e7317b48b6f031
unknown <erwinlopez177@gmail.com>
2024-01-05 19:46:44 -0600

    Commit donde subo ejercicoi de funciones

--- Gen-1-Funciones/src/C1.java
+++ Gen-1-Funciones/src/C1.java
@@ -1,20 +1,20 @@
 public class C1 {
     public static void main(String[] args) {
-
+        System.out.println(suma(2,5));
+        System.out.println(resta(5,10));
+        System.out.println(multiplicacion(2,3));
+        System.out.println(division(10,2));
 
     }
 
     //crear una funcnion que reciba como argumernto dos numeros enteros y regrese la suma de ellos
-    int suma(int a, int b){
-        return a + b;
-    }
+    static int suma(int a, int b){ return a + b; }
     //crear una funcion que recibe como argumento dos numeros enteros y regrese la resta de ellos
-    int resta(int a, int b){
-        return a - b;
-    }
+    static int resta(int a, int b){ return a - b; }
     //crear una funcion que recibe como argumento dos numeros enteros y regrese la multiplicacion de ellos
-    int multiplicacion (int a, int b){ return a * b;}
-
+    static int multiplicacion (int a, int b){ return a * b;}
     //crear una funcion que recibe como argumento dos numeros enteros y regrese la division de ellos
-    double division (int a, int b) { return a / (b * 1.0);}
+    static double division (int a, int b) { return a / (b * 1.0);}
+
+
 }
